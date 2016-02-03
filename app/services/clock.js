dd879f57d5874c1569e6de9ad5171876694a774a
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-02-03 13:31:56 +0100

    services/clock: Add deprecation warning

--- app/services/clock.js
+++ app/services/clock.js
@@ -1,3 +1,17 @@
+import Ember from 'ember';
 import Clock from 'ember-cli-clock/services/legacy';
 
-export default Clock;
+export default Clock.extend({
+  init() {
+    this._super(...arguments);
+    Ember.deprecate(
+      'Usage of the `clock` service is deprecated. ' +
+      'Please generate you own clock service with `ember generate clock-service`.',
+      false,
+      {
+        id: 'ember-cli-clock.deprecate-legacy-clock-service',
+        until: '3.0.0'
+      }
+    );
+  }
+});
