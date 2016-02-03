b5dd036177ecfc4f25dc0736e09c89081c180d3e
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-02-03 15:54:50 +0100

    Use "lolex" library to fake timers in the test code

--- tests/unit/services/clock-test.js
+++ tests/unit/services/clock-test.js
@@ -1,36 +1,41 @@
-import Ember from 'ember';
-
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('service:clock', 'Unit | Service | clock');
+let fakeClock;
+
+moduleFor('service:clock', 'Unit | Service | clock', {
+  beforeEach() {
+    fakeClock = lolex.install();
+  },
+
+  afterEach() {
+    fakeClock.uninstall();
+  }
+});
 
 test('assert that clock will tick two seconds accurately', function(assert) {
-  assert.expect(1);
   var clock = this.subject();
-  var done = assert.async();
-  Ember.run.later(function() {
-    assert.equal(clock.get('second'), 2, 'The clock has ticked twice at 1 second intervals');
-    done();
-  }, 2100);
+  assert.equal(clock.get('second'), 0, 'The clock has not ticked yet');
+
+  fakeClock.tick(2100);
+  assert.equal(clock.get('second'), 2, 'The clock has ticked twice at 1 second intervals');
 });
 
 test('assert that the interval timing can be changed', function(assert) {
-  assert.expect(1);
   var clock = this.subject();
+  assert.equal(clock.get('second'), 0, 'The clock has not ticked yet');
+
   clock.set('intervalTime', 100);
-  var done = assert.async();
-  Ember.run.later(function() {
-    assert.equal(clock.get('second'), 2, 'The clock has ticked twice at 100ms intervals');
-    done();
-  }, 250);
+  assert.equal(clock.get('second'), 0, 'The clock has not ticked yet');
+
+  fakeClock.tick(250);
+  assert.equal(clock.get('second'), 2, 'The clock has ticked twice at 100ms intervals');
 });
 
 test('assert that all counts are incrementing accurately', function(assert) {
-  assert.expect(5);
   var clock = this.subject();
-  for (var i=0; i < 7200; i++) {
-    clock.tick();
-  }
+
+  fakeClock.tick(7200000);
+
   var vals = clock.getProperties('second', 'minute', 'five', 'quarter', 'hour');
   assert.ok(vals.second === 7200, 'Seconds are accurate');
   assert.ok(vals.minute === 120, 'Minutes are accurate');
